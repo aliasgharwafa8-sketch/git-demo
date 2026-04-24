fd74e887a8fa369b82b01b1b837d4a5cf5270051
Wafa Aliasghar <aliasgharwafa8@gmail.com>
2026-04-24 18:38:12 +0500

    ADDjavascript variable notes

---
+++ Desktop/javacript/condisatat.js
@@ -0,0 +1,141 @@
+//  artimetic operators 
+
+//let a = 5;
+//let b = 2;
+//console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a =", a, "& b =", b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+
+//modulus operator
+
+//console.log("a % b =", a % b);
+
+//exponentiation operator
+
+// console.log("a ** b =", a ** b);
+
+// //unary operator
+
+// let a = 5;
+// let b = 2;
+
+//console.log("a =", a, "& b =", b);
+//a++ is increment a-- & this the decrement
+//a++;
+//aa--;
+// a = a - 1;
+//console.log("a =", a);
+// a = a + 1;
+// console.log("a =", a);
+
+//a++ this is the post Increment
+//++a this is the pre increment
+
+//a-- this is the post decrement
+//--a this is the pre decrement
+
+//console.log("a++ = ", a++); //vlue a++ = 5 a++ is first not change the vlue 
+//console.log("a = ", a); // now vlue a = 6 a++ work like that
+
+
+
+//console.log("++a = ", ++a); // vlue ++a = 6 ++a first changed a vlue 
+//console.log("a = ", a); // same vlue  a = 6 same vlue
+
+//--a & a-- same work that i mention in ++a & a++
+
+//assignment operators = += *= %= **=
+
+//this is called compect  you can call it shotcut (a + = 4) means you do like this (a = a = 4)
+
+
+
+// let a = 5;
+// let b = 2;
+
+//a += 4; // it work same a = a + 4
+//console.log("a =", a); // vlue a = 9 a = 5 then a += 4  5 + 4 = 9 it work like this 
+ 
+//a -= 4; // a = a - 4
+//console.log("a =", a); //vlue a = 1 it means 5 - 4 = 1
+
+//a *= 4; // a = a * 4
+//console.log("a =", a);
+
+ //a /= 4; //a = a / 4
+ //console.log("a =", a); // value 1.25 
+
+ //a %= 4; //a = a % 4
+//  console.log("a =", a); //vlue 1
+ 
+ //a **= 4; // a = a ** 4
+  //console.log("a =", a); // vlue a = 625
+
+  //comparison operator/ equal to == equal to & type === not equal to != not equal to & type !==
+  //greater then > greater then equal >= less then > & less then equal >=
+
+//let a = 5;
+//let b = 2;
+
+//console.log("5 == 2", a == b); // is 5 equal to 2 result should be false 5 == 2 false
+//let a = 5;
+//let b = 5;
+//console.log("5 != 5", a != b); // result is false 5 not equal to 5 != 5 flase
+
+//console.log("5 != 2", a != b); // result is true coz 5 is not equal to 2, 5 != 2 true
+
+// === is strict version equal is also check data type string and number nerver compaired lets see how it work
+
+// let a = 5;
+// let b = "5";
+//console.log(" a !== b", a !== b);  true //console.log(" a === b", a === b); result is flase
+
+// let a = 5;
+// let b = 3;
+// console.log("5 > 3", a > b); // result is true 
+ //console.log("5 < 3", a < b); // result is false
+
+//  let a = 5;
+// let b = 5;
+ //console.log("5 <= 5", a <= b); result is true 
+
+ //console.log("5 >= 5", a >= b); result is true
+ //logical operator && || ! there is 3 logical operator
+ //logical operators how they Works, true expresion or true/flase final result we use logic
+ //1st is (logical and &&), when condition 1 is true condition 2 also true result is true  
+
+ //let a =  6;
+ //let b = 5;
+//  let condi1 = a > b;
+//  let condi2 = a === 6;
+//   console.log("codi1 && condi2 =", condi1 && condi2 );// result is true
+ 
+//  let condi1 = a > b;
+//  let condi2 = a === 5;
+//  console.log("codi1 && condi2 =", condi1 && condi2 ); // result is false
+
+ //logical OR || "if at least one of the condiction is true, the result is true"
+// console.log("codi1 || condi2 =", a < b || a === 6 ); result is true
+
+//  logical not ! " it  reverses the value, if the conditon is true,it make it flase ,if the condi is flase, it make it true "
+ 
+ //console.log("!(6<5)=", !(a === 6) ); this is false // console.log("!(6<5)=", !(a < b) ); result is true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
